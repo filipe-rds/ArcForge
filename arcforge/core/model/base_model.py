587510d9d5737da85811541ebbee00b1124b33e1
Gabriel Félix <gfedacs@hotmail.com>
2024-12-03 12:03:54 -0300

    refactor: Remoção da classe Field
    --- Modificações:
    
    ---- Foi retirando a classe Field do arquivo base_model.py, deixando o projeto mais modulariazado.

--- arcforge/core/model/base_model.py
+++ arcforge/core/model/base_model.py
@@ -1,41 +1,7 @@
 import psycopg2
 from psycopg2 import sql
 from config import *
-
-
-# Classe Field para definir campos(ATRIBUTOS) de uma tabela
-class  Field:
-    """Classe base para definir campos."""
-
-    def __init__(self, field_type, primary_key=False, unique=False, nullable=True, default=None):
-        self.field_type = field_type
-        self.primary_key = primary_key
-        self.unique = unique
-        self.nullable = nullable
-        self.default = default
-
-    #Transformando os atributos da classe Field em uma string SQL
-
-    def to_sql(self):
-        sql_definition = self.field_type  # Começa com o tipo de dado
-        
-        # Adiciona a restrição PRIMARY KEY, se houver
-        if self.primary_key:
-            sql_definition += " PRIMARY KEY"
-        
-        # Adiciona UNIQUE, se houver
-        if self.unique:
-            sql_definition += " UNIQUE"
-        
-        # Adiciona NOT NULL, se o campo não for nullable
-        if not self.nullable:
-            sql_definition += " NOT NULL"
-        
-        # Adiciona o valor default, se houver
-        if self.default is not None:
-            sql_definition += f" DEFAULT {self.default}"
-        
-        return sql_definition
+from atributos import *
 
 
 class ForeignKey:

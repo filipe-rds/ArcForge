4d1f64a4f848c6e686246e2d82b33401b3fb81f6
Filipe Rodrigues <rodrigues.filipe@academico.ifpb.edu.br>
2025-02-16 10:20:13 -0300

    refactor: ajuste na conexão para lidar com multi-thread

--- arcforge/core/db/connection.py
+++ arcforge/core/db/connection.py
@@ -1,3 +1,4 @@
+import threading
 import psycopg
 from psycopg import sql
 from contextlib import contextmanager
@@ -20,9 +21,12 @@ logger = logging.getLogger(__name__)
 # -----------------------------------------------------------------------------
 class Singleton(type):
     _instances = {}
+    _lock = threading.Lock()  # Lock para evitar problemas em ambientes multithread
+
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
 
 # -----------------------------------------------------------------------------
